4fd782c78d2bdcf2a6f3fdccd1993dd8a2f61031
Dvirnik Yaroslav <ultve@yandex.ru>
2023-12-02 10:59:47 +0300

    initial base for this project

---
+++ src/test/java/telran/microservises/storehouse/ComputerizedStorehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.microservises.storehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerizedStorehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
