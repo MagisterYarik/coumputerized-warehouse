dee4afd2565592f4e058da53f72b3da1ecf94b52
Dvirnik Yaroslav <ultve@yandex.ru>
2023-12-29 22:05:13 +0300

    container-analyze-close: unit test for appl

--- container-analyzer-close/src/test/java/telran/microservices/analyzer/close/ContainerAnalyzerCloseTest.java
+++ container-analyzer-close/src/test/java/telran/microservices/analyzer/close/ContainerAnalyzerCloseTest.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cloud.stream.binder.test.InputDestination;
