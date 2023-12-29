ee143b290243a1046f16e257d42de6f8cb37d04b
Dvirnik Yaroslav <ultve@yandex.ru>
2023-12-29 20:25:11 +0300

    create-order-request: update appl class

--- create-order-request/src/main/java/telran/microservices/order/request/CreateOrderRequestAppl.java
+++ create-order-request/src/main/java/telran/microservices/order/request/CreateOrderRequestAppl.java
@@ -1,15 +1,46 @@
 package telran.microservices.order.request;
 
+import java.util.function.Consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.context.annotation.Bean;
+
+import telran.coumputerizedWarehouse.dto.ContainerDemand;
+import telran.coumputerizedWarehouse.dto.OrderRequestNew;
+import telran.microservices.order.request.service.CreateOrderRequestNew;
 
 
 @SpringBootApplication
 public class CreateOrderRequestAppl {
+
+
+	@Autowired
+	StreamBridge streamBridge;
+	@Value("${app.analyze.close.producer.binding.name:createOrderRequestNewProducer-out-0}")
+	String bindingName;
+	
+	@Autowired
+	CreateOrderRequestNew createRequest;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CreateOrderRequestAppl.class, args);
 
+	}
+	
+	
+	@Bean
+	Consumer<ContainerDemand> createOrderRequestNewConsumer(){
+		return this::createRequest;
+		
+	}
+	void createRequest(ContainerDemand demand) {
+		OrderRequestNew orderRequest = createRequest.createRequestByDemand(demand);
+		if(orderRequest!=null) 
+			streamBridge.send(bindingName,orderRequest);
 	}
 
 }
