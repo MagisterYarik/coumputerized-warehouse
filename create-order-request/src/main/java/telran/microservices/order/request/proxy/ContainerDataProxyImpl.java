df19ea2877bf21aad68786059783cf73e3009a26
Dvirnik Yaroslav <ultve@yandex.ru>
2023-12-29 20:17:11 +0300

    create-order-request: add classes, impl proxy service

---
+++ create-order-request/src/main/java/telran/microservices/order/request/proxy/ContainerDataProxyImpl.java
@@ -0,0 +1,27 @@
+package telran.microservices.order.request.proxy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import telran.coumputerizedWarehouse.dto.ContainerData;
+
+@Service
+public class ContainerDataProxyImpl implements ContainerDataProxy {
+	@Autowired
+	RestTemplate rest;
+	@Value("${services.url.getContainerData: 'http://localhost:8080'}")
+	String url;
+	@Value("${services.command.getContainerDataById: '/getContainerById/'}")
+	String command;
+	
+	@Override
+	public ContainerData getContainerData(long container_id) {
+		ResponseEntity<ContainerData> response = rest.exchange(url+command+container_id, HttpMethod.GET, null, ContainerData.class);
+		return response.getBody();
+	}
+
+}
---
+++ create-order-request/src/main/java/telran/microservices/order/request/service/CreateOrderRequestNewImpl.java
@@ -0,0 +1,17 @@
+package telran.microservices.order.request.service;
+
+import org.springframework.stereotype.Service;
+
+import telran.coumputerizedWarehouse.dto.ContainerDemand;
+import telran.coumputerizedWarehouse.dto.OrderRequestNew;
+
+@Service
+public class CreateOrderRequestNewImpl implements CreateOrderRequestNew {
+
+	@Override
+	public OrderRequestNew createRequestByDemand(ContainerDemand request_data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
